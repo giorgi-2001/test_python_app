3f303458a4b0a9814f4287026a7d8c1f99c85b5f
giorgi-2001 <gio19900504@gmail.com>
2024-08-14 19:18:33 +0400

    added postgres

--- src/database.py
+++ src/database.py
@@ -1,12 +1,19 @@
 from sqlalchemy import create_engine
 from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
+from os import getenv
 
 
-SQL_ALCHEMY_DB_URL = "sqlite:///./database.db"
+load_dotenv()
 
 
-engine = create_engine(
-    SQL_ALCHEMY_DB_URL, connect_args={"check_same_thread": False})
+SQLITE_URL = "sqlite:///./database.db"
+
+
+DB_URI = getenv("PG_URL", SQLITE_URL)
+
+
+engine = create_engine(DB_URI)
 
 
 SessionLocal = sessionmaker(autoflush=False, autocommit=False, bind=engine)
